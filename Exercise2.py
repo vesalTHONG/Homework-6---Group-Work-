d45031737c02c773c98d134b834d4551c1f515ee
vesalTHONG <102711469+vesalTHONG@users.noreply.github.com>
2022-04-01 21:58:03 +0700

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -1,12 +1,5 @@
 def index_power(array, N):
-    return array[N] ** N if len(array) > N else -1
-
-if __name__ == '__main__':
-    print("example:")
-    print(index_power([102, 98, 77, 45, 109], 3))
-
-    assert index_power([1, 2, 3, 4], 2) == 9, "sqaure"
-    assert index_power([1, 3, 10, 100], 3) == 1000000, "Cube"
-    assert index_power([0, 1], 0) == 1, "Zero power"
-    assert index_power([1, 2], 3) == -1, "IndexError"
-    print("Coding complete")
+    if len(array) > N:
+        return array[N] ** N 
+    else: 
+        return -1
